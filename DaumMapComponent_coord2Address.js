1045d3c5b975f418fb4ddc3bc3e42554d2390a17
KisungKim <31757853+KisungKim@users.noreply.github.com>
2018-09-05 16:16:08 +0900

    Update DaumMapComponent_coord2Address.js

--- DaumMapComponent_coord2Address.js
+++ DaumMapComponent_coord2Address.js
@@ -7,13 +7,15 @@ class Coord2Address extends Component {
     render() {
         let map = this.props.map;
         if(map) {
+            // 받아온 지도 컨테이너의 map에서 현재 중심좌표를 가져옵니다 
             let center = map.getCenter();
             let lng = center.getLng(),
                 lat = center.getLat();
             let messageCoord = "<div>중심좌표를 기준으로 " + lat + " " + lng + "</div>";
             let domObjCoord = document.getElementById("centerCoordFromMapObj");
             domObjCoord.innerHTML = messageCoord;
-
+            
+            // Georecoder() 라이브러리를 사용, coord2Address메소드는 현재 좌표를 기준으로 구/행정동의 정보를 리턴합니다.
             (new daum.maps.services.Geocoder()).coord2Address(lng, lat, function(result, status) {
                 if(status === daum.maps.services.Status.OK) {
                     let messageAddress = "<div>그래서 현재 위치는 " +
